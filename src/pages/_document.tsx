9e192cba63f5ab1665189f459a167e823f65867e
abrahamquerido12 <abrahamquerido@gmail.com>
2023-04-26 09:41:01 -0600

    fix: fixed google maps script loading

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,15 @@
 import { Head, Html, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   const apiKey = process.env.GOOGLE_PLACES_API_KEY;
   return (
     <Html lang="en">
       <Head>
-        {/* 
-      eslint-disable-next-line @next/next/no-sync-scripts */}
-        <script
+        <Script
           type="text/javascript"
-          src={`https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places `}
+          src={`https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places`}
+          strategy="beforeInteractive"
         />
       </Head>
       <body>
