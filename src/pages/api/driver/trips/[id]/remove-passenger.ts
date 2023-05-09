bed65aec2de0016188c0b88349f32fcf37f9dc29
abrahamquerido12 <abrahamquerido@gmail.com>
2023-05-09 11:54:20 -0600

    added notification for removed passenger

--- src/pages/api/driver/trips/[id]/remove-passenger.ts
+++ src/pages/api/driver/trips/[id]/remove-passenger.ts
@@ -1,8 +1,13 @@
 // protected route
+import moment from 'moment';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getServerSession } from 'next-auth/next';
 import prisma from '../../../../../lib/prisma';
+import { sendSms } from '../../../../../lib/twilio';
 import { options } from '../../../auth/[...nextauth]';
+
+const timezone = 'America/Mexico_City';
+
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const session = await getServerSession(req, res, options);
@@ -29,6 +34,21 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         where: {
           passengerId: passengerId,
         },
+        select: {
+          id: true,
+          passenger: {
+            select: {
+              user: {
+                select: { profile: { select: { phoneNumber: true } } },
+              },
+            },
+          },
+          trip: {
+            select: {
+              date: true,
+            },
+          },
+        },
       });
 
       if (!passengerTrip) {
@@ -41,6 +61,19 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
           id: passengerTrip?.id,
         },
       });
+
+      const tripDateTime = moment
+        .tz(passengerTrip?.trip?.date, timezone)
+        .format('DD/MM/YYYY HH:mm');
+
+      const phoneNumber = passengerTrip?.passenger?.user?.profile?.phoneNumber;
+
+      if (process.env.NODE_ENV === 'production' && phoneNumber) {
+        await sendSms(
+          phoneNumber,
+          `Tu viaje del ${tripDateTime} ha sido cancelado`
+        );
+      }
 
       res.status(200).json({
         message: 'Passenger removed',
